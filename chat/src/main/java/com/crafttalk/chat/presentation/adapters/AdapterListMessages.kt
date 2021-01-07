696988ba8bdbb8758a2d0d1abb93f9fb4afa477a
cactus <vt_shustiv@mail.ru>
2021-01-07 03:03:25 +0300

    ShowImageDialog: add enlargement of images

--- chat/src/main/java/com/crafttalk/chat/presentation/adapters/AdapterListMessages.kt
+++ chat/src/main/java/com/crafttalk/chat/presentation/adapters/AdapterListMessages.kt
@@ -23,17 +23,27 @@ class AdapterListMessages(
             R.layout.item_user_text_message -> HolderUserTextMessage(parent.inflate(viewType))
             R.layout.item_server_text_message -> HolderOperatorTextMessage(parent.inflate(viewType), selectAction)
             R.layout.item_user_image_message -> HolderUserImageMessage(parent.inflate(viewType), updateData)
-            { imageUrl, width, height -> openImage(parent.context as Activity, imageUrl) }
+            { imageUrl -> openImage(parent.context as Activity, imageUrl) }
             R.layout.item_server_image_message -> HolderOperatorImageMessage(parent.inflate(viewType), updateData)
-            { imageUrl, width, height -> openImage(parent.context as Activity, imageUrl) }
+            { imageUrl -> openImage(parent.context as Activity, imageUrl) }
             R.layout.item_user_file_message -> HolderUserFileMessage(parent.inflate(viewType))
             { fileUrl -> openFile(parent.context, fileUrl) }
             R.layout.item_server_file_message -> HolderOperatorFileMessage(parent.inflate(viewType))
             { fileUrl -> openFile(parent.context, fileUrl) }
             R.layout.item_user_gif_message -> HolderUserGifMessage(parent.inflate(viewType), updateData)
-            { gifUrl, width, height -> openGif(parent.context as Activity, gifUrl) }
+            { gifUrl -> openGif(parent.context as Activity, gifUrl) }
             R.layout.item_server_gif_message -> HolderOperatorGifMessage(parent.inflate(viewType), updateData)
-            { gifUrl, width, height -> openGif(parent.context as Activity, gifUrl) }
+            { gifUrl -> openGif(parent.context as Activity, gifUrl) }
+            R.layout.item_user_union_message -> HolderUserUnionMessage(parent.inflate(viewType), updateData,
+                { gifUrl -> openGif(parent.context as Activity, gifUrl) },
+                { imageUrl -> openImage(parent.context as Activity, imageUrl) },
+                { fileUrl -> openFile(parent.context, fileUrl) }
+            )
+            R.layout.item_server_union_message -> HolderOperatorUnionMessage(parent.inflate(viewType), selectAction, updateData,
+                { gifUrl -> openGif(parent.context as Activity, gifUrl) },
+                { imageUrl -> openImage(parent.context as Activity, imageUrl) },
+                { fileUrl -> openFile(parent.context, fileUrl) }
+            )
             else -> HolderDefaultMessage(parent.inflate(R.layout.item_default_message))
         }
     }
