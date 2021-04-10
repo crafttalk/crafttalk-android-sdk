ba1cf2d530318d0c1b36f90e6a5afa02ecafd4d4
cactus <vt_shustiv@mail.ru>
2021-04-11 00:52:07 +0300

    Fixed a defect with logging out without entering the chat

--- chat/src/main/java/com/crafttalk/chat/initialization/Chat.kt
+++ chat/src/main/java/com/crafttalk/chat/initialization/Chat.kt
@@ -18,17 +18,17 @@ object Chat {
     private val scopeIO = CoroutineScope(Dispatchers.IO + job)
     private val scopeUI = CoroutineScope(Dispatchers.Main + job)
 
-    private lateinit var customizingChatBehaviorInteractor: CustomizingChatBehaviorInteractor
-    private lateinit var visitorInteractor: VisitorInteractor
-    private lateinit var authInteractor: AuthInteractor
-    private lateinit var notificationInteractor: NotificationInteractor
-    private lateinit var personInteractor: PersonInteractor
+    private var customizingChatBehaviorInteractor: CustomizingChatBehaviorInteractor? = null
+    private var visitorInteractor: VisitorInteractor? = null
+    private var authInteractor: AuthInteractor? = null
+    private var notificationInteractor: NotificationInteractor? = null
+    private var personInteractor: PersonInteractor? = null
     private var sdkComponent: SdkComponent? = null
 
     internal fun getSdkComponent(): SdkComponent = sdkComponent ?: throw IllegalStateException("You must call the init method before going to the chat.")
 
     fun setOnChatMessageListener(listener: ChatMessageListener) {
-        customizingChatBehaviorInteractor.setMessageListener(listener)
+        customizingChatBehaviorInteractor?.setMessageListener(listener)
     }
 
     private fun initDI(context: Context) {
@@ -39,9 +39,9 @@ object Chat {
         }
         customizingChatBehaviorInteractor = CustomizingChatBehaviorInteractor(sdkComponent!!.getChatBehaviorRepository())
         visitorInteractor = VisitorInteractor(sdkComponent!!.getVisitorRepository())
-        notificationInteractor = NotificationInteractor(sdkComponent!!.getNotificationRepository(), visitorInteractor)
         personInteractor = PersonInteractor(sdkComponent!!.getPersonRepository())
-        authInteractor = AuthInteractor(sdkComponent!!.getAuthRepository(), visitorInteractor, personInteractor, notificationInteractor)
+        notificationInteractor = NotificationInteractor(sdkComponent!!.getNotificationRepository(), visitorInteractor!!)
+        authInteractor = AuthInteractor(sdkComponent!!.getAuthRepository(), visitorInteractor!!, personInteractor!!, notificationInteractor!!)
     }
 
     private fun initParams(
@@ -68,26 +68,26 @@ object Chat {
     }
 
     fun wakeUp(visitor: Visitor?) {
-        customizingChatBehaviorInteractor.openApp()
-        authInteractor.logIn(
+        customizingChatBehaviorInteractor?.openApp()
+        authInteractor?.logIn(
             visitor = visitor
         )
     }
 
     fun destroy() {
-        customizingChatBehaviorInteractor.closeApp()
-        customizingChatBehaviorInteractor.destroyHostChat()
+        customizingChatBehaviorInteractor?.closeApp()
+        customizingChatBehaviorInteractor?.destroyHostChat()
     }
 
     fun logOut(context: Context) {
         scopeIO.launch {
-            authInteractor.logOut(context.filesDir)
+            authInteractor?.logOut(context.filesDir)
         }
     }
 
     fun logOutWithUIActionAfter(context: Context, actionUIAfterLogOut: () -> Unit) {
         scopeIO.launch {
-            authInteractor.logOut(context.filesDir)
+            authInteractor?.logOut(context.filesDir)
             scopeUI.launch {
                 actionUIAfterLogOut()
             }
@@ -96,7 +96,7 @@ object Chat {
 
     fun logOutWithIOActionAfter(context: Context, actionIOAfterLogOut: () -> Unit) {
         scopeIO.launch {
-            authInteractor.logOut(context.filesDir)
+            authInteractor?.logOut(context.filesDir)
             actionIOAfterLogOut()
         }
     }
