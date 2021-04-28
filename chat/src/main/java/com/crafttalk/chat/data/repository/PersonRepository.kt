b5f013cb4f22991afabc78d3e339b8aca0621b15
cactus <vt_shustiv@mail.ru>
2021-04-28 17:36:51 +0300

    Added processing of unsuccessful operator preview request

--- chat/src/main/java/com/crafttalk/chat/data/repository/PersonRepository.kt
+++ chat/src/main/java/com/crafttalk/chat/data/repository/PersonRepository.kt
@@ -1,6 +1,7 @@
 package com.crafttalk.chat.data.repository
 
 import android.database.sqlite.SQLiteConstraintException
+import android.util.Log
 import com.crafttalk.chat.data.api.rest.PersonApi
 import com.crafttalk.chat.data.helper.network.toData
 import com.crafttalk.chat.data.local.db.dao.MessagesDao
@@ -9,6 +10,7 @@ import com.crafttalk.chat.data.local.db.entity.PersonEntity
 import com.crafttalk.chat.domain.repository.IPersonRepository
 import com.crafttalk.chat.utils.ChatAttr
 import com.crafttalk.chat.utils.OperatorPreviewMode
+import java.lang.Exception
 import javax.inject.Inject
 
 class PersonRepository
@@ -18,27 +20,32 @@ class PersonRepository
     private val personApi: PersonApi
 ) : IPersonRepository {
     override fun getPersonPreview(personId: String, visitorToken: String): String? {
-        when (ChatAttr.getInstance().operatorPreviewMode) {
-            OperatorPreviewMode.CACHE_ONLY_LINK -> {
-                return personDao.getPersonPreview(personId)
-                    ?: personApi.getPersonPreview(
+        try {
+            when (ChatAttr.getInstance().operatorPreviewMode) {
+                OperatorPreviewMode.CACHE_ONLY_LINK -> {
+                    return personDao.getPersonPreview(personId)
+                        ?: personApi.getPersonPreview(
+                            personId = personId,
+                            visitorToken = visitorToken
+                        ).toData().picture?.apply {
+                            try {
+                                personDao.addPersonPreview(PersonEntity(personId, this))
+                                messagesDao.updatePersonPreview(personId, this)
+                            } catch (ex: SQLiteConstraintException) {}
+                        }
+                }
+                OperatorPreviewMode.ALWAYS_REQUEST -> {
+                    return personApi.getPersonPreview(
                         personId = personId,
                         visitorToken = visitorToken
-                    ).toData().picture?.apply {
-                        try {
-                            personDao.addPersonPreview(PersonEntity(personId, this))
-                            messagesDao.updatePersonPreview(personId, this)
-                        } catch (ex: SQLiteConstraintException) {}
+                    ).toData().picture.apply {
+                        messagesDao.updatePersonPreview(personId, this)
                     }
-            }
-            OperatorPreviewMode.ALWAYS_REQUEST -> {
-                return personApi.getPersonPreview(
-                    personId = personId,
-                    visitorToken = visitorToken
-                ).toData().picture.apply {
-                    messagesDao.updatePersonPreview(personId, this)
                 }
             }
+        } catch (ex: Exception) {
+            Log.e("FAIL_REQUEST", "getPersonPreview fail: ${ex.message}")
+            return null
         }
     }
 }
