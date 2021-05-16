5eb8c71ca9a12e79e28a533eb03d6136a621fd57
cactus <vt_shustiv@mail.ru>
2021-05-16 21:23:32 +0300

    Fixed issue with missing file information in messages like file + text messages

--- chat/src/main/java/com/crafttalk/chat/presentation/helper/ui/media.kt
+++ chat/src/main/java/com/crafttalk/chat/presentation/helper/ui/media.kt
@@ -6,8 +6,8 @@ import android.graphics.drawable.Drawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
-import java.lang.Exception
 import java.net.URL
+import kotlin.math.min
 
 fun getSizeMediaFile(context: Context, url: String, resultSize: (height: Int?, width: Int?) -> Unit) {
     Glide.with(context)
@@ -26,13 +26,36 @@ fun getSizeMediaFile(context: Context, url: String, resultSize: (height: Int?, w
 }
 
 fun getWeightFile(urlPath: String, resultSize: (size: Long) -> Unit) {
+    val CONTENT_DISPOSITION = "content-disposition"
+    val template = "size="
+
     return try {
         val url = URL(urlPath)
         val urlConnection = url.openConnection()
         urlConnection.connect()
         val size = urlConnection.contentLength
-        resultSize(size.toLong())
+
+        if (size == -1) {
+            val contentDisposition = urlConnection.getHeaderField(CONTENT_DISPOSITION)
+            if (contentDisposition == null) {
+                resultSize(-1)
+            } else {
+                val startIndex = contentDisposition.indexOf(template) + template.length
+                val indexEndComma = contentDisposition.indexOf(",", startIndex)
+                val indexEndBracket = contentDisposition.indexOf("]", startIndex)
+                val alternativeSize = (when {
+                    startIndex != -1 && indexEndComma != -1 && indexEndBracket != -1 -> contentDisposition.substring(startIndex, min(indexEndComma, indexEndBracket))
+                    startIndex != -1 && indexEndComma != -1 && indexEndBracket == -1 -> contentDisposition.substring(startIndex, indexEndComma)
+                    startIndex != -1 && indexEndComma == -1 && indexEndBracket != -1 -> contentDisposition.substring(startIndex, indexEndBracket)
+                    startIndex != -1 && indexEndComma == -1 && indexEndBracket == -1 -> contentDisposition.substring(startIndex)
+                    else -> "-1"
+                }).toLong()
+                resultSize(alternativeSize)
+            }
+        } else {
+            resultSize(size.toLong())
+        }
     } catch (ex: Exception) {
-        resultSize(0)
+        resultSize(-1)
     }
 }
