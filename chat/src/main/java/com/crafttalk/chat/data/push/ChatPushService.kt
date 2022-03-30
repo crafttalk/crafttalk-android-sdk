d47fd1c827421137090ba25ac80815cddb03d786
Alexander <Sash0k@users.noreply.github.com>
2022-03-30 15:53:53 +0300

    сделал ChatPushService открытым

--- chat/src/main/java/com/crafttalk/chat/data/push/ChatPushService.kt
+++ chat/src/main/java/com/crafttalk/chat/data/push/ChatPushService.kt
@@ -2,4 +2,4 @@ package com.crafttalk.chat.data.push
 
 import com.google.firebase.messaging.FirebaseMessagingService
 
-class ChatPushService: FirebaseMessagingService()
+open class ChatPushService: FirebaseMessagingService()
