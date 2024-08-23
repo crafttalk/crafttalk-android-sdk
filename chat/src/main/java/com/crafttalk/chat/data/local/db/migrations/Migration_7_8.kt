faef0b3e8be1fc2a8dd2930e45ee20a5824c014d
philipok2 <118670901+philipok2@users.noreply.github.com>
2024-08-23 16:36:23 +0300

    DB update

--- chat/src/main/java/com/crafttalk/chat/data/local/db/migrations/Migration_7_8.kt
+++ chat/src/main/java/com/crafttalk/chat/data/local/db/migrations/Migration_7_8.kt
@@ -2,9 +2,10 @@ package com.crafttalk.chat.data.local.db.migrations
 
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.crafttalk.chat.data.local.db.entity.MessageEntity
 
 object Migration_7_8: Migration(7,8) {
     override fun migrate(database: SupportSQLiteDatabase) {
-
+        database.execSQL("DELETE FROM " +  MessageEntity.TABLE_NAME);
     }
 }
